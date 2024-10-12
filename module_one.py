874c14c40db5e664af0155898069a59b17bd5de0
robdf <sdf>
2024-10-11 21:27:22 -0500

    module_one

---
+++ module_one.py
@@ -0,0 +1,16 @@
+def add(num1, num2):
+    return num1 + num2
+
+print(add(8, 2))
+
+def subtract(num1, num2):
+    return num1 - num2
+print(subtract(47, 2))
+
+def multiply(num1, num2):
+    return num1 * num2
+print(multiply(7, 3))
+
+def divide(num1, num2):
+    return num1 / num2
+print(divide(4, 2))
